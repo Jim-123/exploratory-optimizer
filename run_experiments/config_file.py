f5aa6238f418dc1521f7d427f0273bbe14e64a6e
jim <1183520106@qq.com>
2023-11-21 13:28:35 +0800

    Update config_file.py

--- run_experiments/config_file.py
+++ run_experiments/config_file.py
@@ -2,11 +2,11 @@
 # database connection information
 
 # your database information
-db_database = "imdb"  # imdb postgres
-db_user_name = "postgres"
-db_passwd = "1122"
-db_host = "10.68.74.239"
-db_port = "59998"
+db_database = ""  # imdb postgres
+db_user_name = ""
+db_passwd = ""
+db_host = ""
+db_port = ""
 query_file_name = "/imdb/uniform/imdb_queries_from_all_33_query_templates.csv"
 run_result_name = "imdb_queries_from_all_33_query_template_runtimes"
 num_query_template = 33
