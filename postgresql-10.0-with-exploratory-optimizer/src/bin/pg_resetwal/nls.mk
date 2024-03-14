9e4f2e2a2bc16e2a7c0c0de17296403323521013
Jim-123 <1183520106@qq.com>
2024-03-14 20:47:57 +0800

    remove old

--- postgresql-10.0-with-exploratory-optimizer/src/bin/pg_resetwal/nls.mk
+++
@@ -1,4 +0,0 @@
-# src/bin/pg_resetwal/nls.mk
-CATALOG_NAME     = pg_resetwal
-AVAIL_LANGUAGES  = de es fr ja ko pl pt_BR ru sv zh_CN
-GETTEXT_FILES    = pg_resetwal.c ../../common/restricted_token.c
